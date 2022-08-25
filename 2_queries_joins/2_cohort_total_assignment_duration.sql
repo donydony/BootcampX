60a33a6edad009bc142cbeaa78a86ff6f9120d9f
Donny Zhao <donny.zh@hotmail.com>
2022-08-24 23:37:39 -0400

    completed 2_queries_joins

---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,4 @@
+SELECT sum(assignment_submissions.duration)
+FROM (assignment_submissions JOIN students ON student_id = students.id)
+JOIN cohorts on cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
